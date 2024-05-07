2da50fb23770b7d8d604361b71ce6cae8812a90f
JesusF10 <jesusfl06oqk@gmail.com>
2024-05-07 00:25:24 -0700

    Ahora el main contiene uan funcion que genera un triangulo de pascal.

---
+++ main.py
@@ -0,0 +1,13 @@
+
+def draw_triangle(n: int) -> list:
+    """
+    Returns the pascal triangle with n rows
+    """
+    assert n < 0 : ValueError
+    row = [[1]]
+    for i in range(1, n):
+        new_row = [1]
+        for j in range(2, i):
+            new_row.append(row[i - 1][j - 2] + row[i - 1][j - 1])
+        row.append(new_row)
+    return row
